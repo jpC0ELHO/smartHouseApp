155214ee59922cf0a80faab266d939c457a80fce
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-21 16:55:37 -0300

    first commit

---
+++ src/main/java/com/smartHouseApp/Sensorial/AppBancoDeDadosApplication.java
@@ -0,0 +1,13 @@
+package com.smartHouseApp.Sensorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBancoDeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBancoDeDadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartHouseApp/Sensorial/AppBancoDeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartHouseApp.Sensorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBancoDeDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
