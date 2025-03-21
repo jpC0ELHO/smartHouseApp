87433f50f2aa061d19ef465d550d12ec9db78fa8
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-03-21 17:11:15 -0300

    created Regiao.java

---
+++ src/main/java/com/smartHouseApp/Sensorial/domain/entities/enums/Regiao.java
@@ -0,0 +1,34 @@
+package com.smartHouseApp.Sensorial.domain.entities.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Regiao {
+    SALA("SALA"),
+    QUARTO1("QUARTO1J"),
+    QUARTO2("QUARTO2F"),
+    ESCRITORIO("ESCRITORIO"),
+    COZINHA("COZINHA"),
+    BANHEIRO1("BANHEIRO1"),
+    BANHEIRO2("BANHEIRO2F"),
+    GARAGEM("GARAGEM"),
+    CORREDOR_GARAGEM("CORREDOR GARAGEM");
+    private final String descricao;
+
+    Regiao(String descricao){
+        this.descricao=descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public static Regiao fromLocalidade(String descricao){
+        for (Regiao local: Regiao.values()){
+            if (local.getDescricao().equalsIgnoreCase(descricao)){
+                return local;
+            }
+        }
+        throw new IllegalArgumentException("Localidade invalida: "+descricao);
+    }
+}
